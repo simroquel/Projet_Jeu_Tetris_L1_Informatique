976bb1a91bd1a8f7e8dee3be675135bc4b22e234
simroquel <sim.roquel@gmail.com>
2026-03-18 15:54:05 +0100

    Add class Position

--- Iteration0/Tetris/NoyauTetris/NoyauTetris.cs
+++ Iteration0/Tetris/NoyauTetris/NoyauTetris.cs
@@ -16,3 +16,26 @@ public class JeuTetris
     public static int HauteurGrille = 15;
 
 }
+/** Définit la position d'un carré avec ses positions x et y*/
+public class Position
+{
+    public int x;
+    public int y;
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public void DeplacerGauche()
+    {
+        x = x - 1;
+    }
+    public void DeplacerDroite()
+    {
+        x = x + 1;
+    }
+    public void DeplacerBas()
+    {
+        y = y + 1;
+    }
+}
