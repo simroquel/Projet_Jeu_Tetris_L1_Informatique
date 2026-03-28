d28b6d55c67aec18b8244641f6c5222bab1bdf27
simroquel <sim.roquel@gmail.com>
2026-03-28 12:51:31 +0100

    rajout de la doc

--- Iteration0/Tetris/InterfaceTetris/MainWindow.axaml.cs
+++ Iteration0/Tetris/InterfaceTetris/MainWindow.axaml.cs
@@ -41,28 +41,30 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        
+        /** Définit la taille de la fenêtre à partir des constantes*/
         Width = 300;
         Height = 600;
+        /** Définit le texte de InfoText*/
         InfoText.Text = "Jeu Tetris de l'équipe 3MS";
-        
+        /** Définit la taille du canvas à partir de la largeur de la grille et de la taille des carrés*/
         TetrisCanvas.Width = JeuTetris.LargeurGrille * TailleCarre;
         TetrisCanvas.Height = JeuTetris.HauteurGrille * TailleCarre;
-        
+        /** Définit la taille des boutons à partir de la largeur du canvas*/
         StartButton.Width = TetrisCanvas.Width;
         StartButton.Height = 35;
         QuitButton.Width = TetrisCanvas.Width;
         QuitButton.Height = 35; 
-
+        /** Initialise le minuteur pour faire descendre le tetrino courant toutes les 500 milisecondes*/
         Minuteur = new DispatcherTimer();
         Minuteur.Interval = TimeSpan.FromMilliseconds(500);
         Minuteur.Tick += (s, e) => { BasInterface(); };   
-
+        /** détecte le clic sur le bouton Démarrer, déclanche l'évènement DemarrerInterface, puis appelle la méthode DemarrerTetris*/
         StartButton.Click += (s, e) => { DemarrerInterface(); };
+        /** détecte le clic sur le bouton Quitter, déclanche l'évènement Quiter, puis ferme la fenêtre*/
         QuitButton.Click += (s, e) => { Close(); };
-
+        /** Initialisation de l'instance du jeu*/
         Jeu = new JeuTetris();
-
+        /** détecte la pression d'une touche du clavier, et déclanche l'évènement correspondant*/
         KeyDown += (s, e) =>
         {
             if (e.Key == Key.Left) GaucheInterface();
@@ -80,7 +82,6 @@ public partial class MainWindow : Window
     {
         TetrisCanvas.Children.Clear();
         DessinerCadre(); 
-        
         Position[] positionsDuTetrino = Jeu.TetrinoCourant.Positions();
         foreach (Position p in positionsDuTetrino)
         {
