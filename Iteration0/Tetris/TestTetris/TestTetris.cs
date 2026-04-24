c9622f45e0e8c661183b010f35c3c80ac8b03e37
simroquel <sim.roquel@gmail.com>
2026-04-24 22:51:58 +0200

    Update TestTetris.cs

--- Iteration0/Tetris/TestTetris/TestTetris.cs
+++ Iteration0/Tetris/TestTetris/TestTetris.cs
@@ -18,6 +18,7 @@ public class TestJeuTetris
     {
         JeuTetris jeu = new JeuTetris();
         jeu.Demarrer();
+        jeu.TetrinoCourant.PositionOrigine = new Position(0,0);
         int posXAvant = jeu.TetrinoCourant.PositionOrigine.x;
         jeu.Gauche();
         Assert.Equal(posXAvant , jeu.TetrinoCourant.PositionOrigine.x);
@@ -28,7 +29,9 @@ public class TestJeuTetris
     {
         JeuTetris jeu = new JeuTetris();
         jeu.Demarrer();
-        int posXDebut = jeu.TetrinoCourant.PositionOrigine.x + 1;
+        jeu.TetrinoCourant.Indice = 0;
+        jeu.TetrinoCourant.PositionOrigine = new Position(5,3);
+        int posXDebut = jeu.TetrinoCourant.PositionOrigine.x;
         jeu.Gauche();
         Assert.Equal(posXDebut - 1 , jeu.TetrinoCourant.PositionOrigine.x);
     }
@@ -50,19 +53,19 @@ public class TestJeuTetris
         JeuTetris jeu = new JeuTetris();
         jeu.Demarrer();
         int posXAvant = jeu.TetrinoCourant.PositionOrigine.x;
+        if(posXAvant == JeuTetris.LargeurGrille - 1){posXAvant = posXAvant - 1;}
         jeu.Droite();
         Assert.Equal(posXAvant + 1 , jeu.TetrinoCourant.PositionOrigine.x);
     }
     [Fact]
-    /**Teste si Bas(), quand le Tetrino est à la limite, crée un nouveau Tetrino d'origine (0,0)*/
+    /**Teste si Bas(), quand le Tetrino est à la limite, crée un nouveau Tetrino d'ordonnée 0*/
     public void TestBas_Limite()
     {
         JeuTetris jeu = new JeuTetris();
         jeu.Demarrer();
-        jeu.TetrinoCourant.PositionOrigine.y = jeu.TetrinoCourant.PositionOrigine.y + JeuTetris.HauteurGrille - 1;
+        jeu.TetrinoCourant.PositionOrigine.y = JeuTetris.HauteurGrille - 1;
         jeu.Bas();
         Assert.Equal(0, jeu.TetrinoCourant.PositionOrigine.y);
-        Assert.Equal(0, jeu.TetrinoCourant.PositionOrigine.x);
     }
     [Fact]
     /**Teste si Bas() déplace le Tetrino en bas quand il n'est pas à la limite*/
@@ -71,8 +74,10 @@ public class TestJeuTetris
         JeuTetris jeu = new JeuTetris();
         jeu.Demarrer();
         int posYAvant = jeu.TetrinoCourant.PositionOrigine.y;
+        int posXAvant = jeu.TetrinoCourant.PositionOrigine.x;
         jeu.Bas();
         Assert.Equal(posYAvant + 1, jeu.TetrinoCourant.PositionOrigine.y);
+        Assert.Equal(posXAvant, jeu.TetrinoCourant.PositionOrigine.x); // On regarde que X n'a pas été modifié
     }
 }
 public class TestPosition()
